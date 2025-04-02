6a396feb90696b7275d3e85e98c7f4282c3ad045
Deeppatel91 <deeppatel9171@gmail.com>
2025-04-02 16:08:32 -0400

    fixxxx

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,23 @@
 import { Routes } from '@angular/router';
 import { MissionListComponent } from './components/mission-list/mission-list.component';
 import { MissionDetailsComponent } from './components/mission-details/mission-details.component';
-
 export const routes: Routes = [
   { path: '', component: MissionListComponent },
-  { 
-    path: 'missions/:flightNumber', 
-    component: MissionDetailsComponent 
-    // No prerender config (renders dynamically at runtime)
+  {
+    path: 'missions/:flightNumber',
+    component: MissionDetailsComponent,
+    providers: [
+      {
+        provide: 'getPrerenderParams',
+        useValue: () => {
+          return [
+            { flightNumber: '1' },
+            { flightNumber: '2' },
+            // Add other flight numbers you want prerendered
+          ];
+        }
+      }
+    ]
   },
   { path: '**', redirectTo: '' }
 ];
